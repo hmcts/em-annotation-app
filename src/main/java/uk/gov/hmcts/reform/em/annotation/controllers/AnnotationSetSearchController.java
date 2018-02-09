c40670311aafd7333696d15da7a26b6d04f03e18
Louis Houghton <louis@louishoughton.com>
2018-02-09 11:35:52 +0000

    Use correct RequestParam annotation (#5)
    * Use correct RequestParam annotation
    
    * Fix imports

--- src/main/java/uk/gov/hmcts/reform/em/annotation/controllers/AnnotationSetSearchController.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/controllers/AnnotationSetSearchController.java
@@ -7,18 +7,16 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.em.annotation.domain.AnnotationSet;
 import uk.gov.hmcts.reform.em.annotation.service.StoredAnnotationSetSearchService;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping(
@@ -39,7 +37,7 @@ public class AnnotationSetSearchController {
         @ApiResponse(code = 200, message = "Success", response = PagedResources.class)
     })
     public ResponseEntity<PagedResources<Resource<AnnotationSet>>> findAllAnnotationSetByDocumentUrl(
-        @Valid @Param("url") String url,
+        @RequestParam("url") String url,
         Pageable pageable,
         PagedResourcesAssembler<AnnotationSet> assembler) {
 
