353279e01373a6449c92ec72fba61ea01c0dff3d
Louis Houghton <louis@louishoughton.com>
2018-02-12 15:56:45 +0000

    optimise imports

--- src/test/java/uk/gov/hmcts/reform/em/annotation/controllers/StoredAnnotationSetControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/annotation/controllers/StoredAnnotationSetControllerTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.em.annotation.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
