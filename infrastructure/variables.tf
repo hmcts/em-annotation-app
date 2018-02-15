1a56fa1eb137c2271fbb91622d8dbe83bc7bbd74
Alec Doran-Twyford <alec@alectronic.co>
2018-02-15 22:35:05 +0000

    cnp changes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,15 +4,23 @@ variable "product" {
 }
 
 variable "location" {
-    type    = "string"
-    default = "UK South"
+  type    = "string"
+  default = "UK South"
 }
 
 variable "env" {
-    type = "string"
+  type = "string"
 }
 
-variable "infrastructure_env" {
-    default     = "dev"
-    description = "Infrastructure environment to point to"
+variable "subscription" {
+  type = "string"
+}
+
+variable "ilbIp"{}
+
+variable "tenant_id" {}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
