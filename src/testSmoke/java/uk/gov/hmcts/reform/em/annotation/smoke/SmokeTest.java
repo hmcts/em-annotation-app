2570d8c849f290a8a01801e5775f9b94eb64de8c
Alec Doran-Twyford <alec@alectronic.co>
2018-02-19 19:16:11 +0000

    add dumby smoke & intergation test for cnp

---
+++ src/testSmoke/java/uk/gov/hmcts/reform/em/annotation/smoke/SmokeTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.em.annotation.smoke;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SmokeTest {
+
+    @Test
+    public void simple_test() {
+        Assert.assertTrue(true);
+    }
+
+}
