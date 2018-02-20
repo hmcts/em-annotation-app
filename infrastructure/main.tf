5fd5aca9f50cbb8bc4284819ebec7a6073c622e5
Alec Doran-Twyford <alec@alectronic.co>
2018-02-20 13:22:54 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,31 +1,30 @@
 module "app" {
-  source   = "git@github.com:contino/moj-module-webapp?ref=master"
-  product  = "${var.product}-${var.app_name}-${var.app_type}"
+  source = "git@github.com:contino/moj-module-webapp?ref=master"
+  product = "${var.product}-${var.app_name}-${var.app_type}"
   location = "${var.location}"
-  env      = "${var.env}"
-  ilbIp    = "${var.ilbIp}"
+  env = "${var.env}"
+  ilbIp = "${var.ilbIp}"
 
   app_settings = {
-    POSTGRES_HOST     = "${module.db.host_name}"
-    POSTGRES_PORT     = "${module.db.postgresql_listen_port}"
+    POSTGRES_HOST = "${module.db.host_name}"
+    POSTGRES_PORT = "${module.db.postgresql_listen_port}"
     POSTGRES_DATABASE = "${module.db.postgresql_database}"
-    POSTGRES_USER     = "${module.db.user_name}"
+    POSTGRES_USER = "${module.db.user_name}"
     POSTGRES_PASSWORD = "${module.db.postgresql_password}"
 
+    # JAVA_OPTS = "${var.java_opts}"
+    # SERVER_PORT = "8080"
 
-    JAVA_OPTS =  "${var.java_opts}"
-    SERVER_PORT = "8080"
-
-    // db
+    # db
     SPRING_DATASOURCE_URL = "jdbc:postgresql://${module.db.host_name}:${module.db.postgresql_listen_port}/${module.db.postgresql_database}"
     SPRING_DATASOURCE_USERNAME = "${module.db.user_name}"
     SPRING_DATASOURCE_PASSWORD = "${module.db.postgresql_password}"
 
-    // idam
+    # idam
     IDAM_CLIENT_URL = "${var.idam-api-url}"
     PROVIDER_SERVICE_CLIENT_URL = "${var.s2s-url}"
 
-    //   logging vars & healthcheck
+    # logging vars & healthcheck
     REFORM_SERVICE_NAME = "${var.product}-${var.app_name}-${var.app_type}"
     REFORM_TEAM = "${var.team_name}"
     REFORM_SERVICE_TYPE = "${var.app_language}"
@@ -35,66 +34,66 @@ module "app" {
     PACKAGES_PROJECT = "${var.team_name}"
     PACKAGES_ENVIRONMENT = "${var.env}"
 
-    ROOT_APPENDER =  "${var.root_appender}"
-    JSON_CONSOLE_PRETTY_PRINT =  "${var.json_console_pretty_print}"
-    LOG_OUTPUT =  "${var.log_output}"
-    ROOT_LOGGING_LEVEL =  "${var.root_logging_level}"
-    LOG_LEVEL_SPRING_WEB =  "${var.log_level_spring_web}"
+    ROOT_APPENDER = "${var.root_appender}"
+    JSON_CONSOLE_PRETTY_PRINT = "${var.json_console_pretty_print}"
+    LOG_OUTPUT = "${var.log_output}"
+    ROOT_LOGGING_LEVEL = "${var.root_logging_level}"
+    LOG_LEVEL_SPRING_WEB = "${var.log_level_spring_web}"
     LOG_LEVEL_DM = "${var.log_level_dm}"
     SHOW_SQL = "${var.show_sql}"
 
     ENDPOINTS_HEALTH_SENSITIVE = "${var.endpoints_health_sensitive}"
     ENDPOINTS_INFO_SENSITIVE = "${var.endpoints_info_sensitive}"
 
-//    S2S_NAMES_WHITELIST = "${var.s2s_names_whitelist}"
-//    CASE_WORKER_ROLES = "${var.case_worker_roles}"
+    # S2S_NAMES_WHITELIST = "${var.s2s_names_whitelist}"
+    # CASE_WORKER_ROLES = "${var.case_worker_roles}"
   }
 }
 
 module "db" {
-  source              = "git@github.com:contino/moj-module-postgres?ref=master"
-  product             = "${var.product}-${var.app_name}-postgres-db"
-  location            = "West Europe"
-  env                 = "${var.env}"
-  postgresql_user     = "rhubarbadmin"
+  source = "git@github.com:contino/moj-module-postgres?ref=master"
+  product = "${var.product}-${var.app_name}-postgres-db"
+  location = "West Europe"
+  env = "${var.env}"
+  postgresql_user = "rhubarbadmin"
 }
 
 module "key-vault" {
-  source              = "git@github.com:contino/moj-module-key-vault?ref=master"
-  product             = "${var.product}-${var.app_name}-${var.app_type}"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
+  source = "git@github.com:contino/moj-module-key-vault?ref=master"
+  product = "${var.product}-${var.app_name}-${var.app_type}"
+  env = "${var.env}"
+  tenant_id = "${var.tenant_id}"
+  object_id = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${module.app.resource_group_name}"
   product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name      = "${var.product}-${var.app_name}-POSTGRES-USER"
-  value     = "${module.db.user_name}"
+  name = "${var.product}-${var.app_name}-POSTGRES-USER"
+  value = "${module.db.user_name}"
   vault_uri = "${module.key-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name      = "${var.product}-${var.app_name}-POSTGRES-PASS"
-  value     = "${module.db.postgresql_password}"
+  name = "${var.product}-${var.app_name}-POSTGRES-PASS"
+  value = "${module.db.postgresql_password}"
   vault_uri = "${module.key-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name      = "${var.product}-${var.app_name}-POSTGRES-HOST"
-  value     = "${module.db.host_name}"
+  name = "${var.product}-${var.app_name}-POSTGRES-HOST"
+  value = "${module.db.host_name}"
   vault_uri = "${module.key-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name      = "${var.product}-${var.app_name}-POSTGRES-PORT"
-  value     = "${module.db.postgresql_listen_port}"
+  name = "${var.product}-${var.app_name}-POSTGRES-PORT"
+  value = "${module.db.postgresql_listen_port}"
   vault_uri = "${module.key-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name      = "${var.app_name}-POSTGRES-DATABASE"
-  value     = "${module.db.postgresql_database}"
+  name = "${var.product}-${var.app_name}-POSTGRES-DATABASE"
+  value = "${module.db.postgresql_database}"
   vault_uri = "${module.key-vault.key_vault_uri}"
 }
