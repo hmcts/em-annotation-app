f2fa5da5babfc9341552adc8763142ab588027c3
Alec Doran-Twyford <alec@alectronic.co>
2018-04-11 16:12:32 +0100

    s2s change

--- functionalTests/src/functionalTest/java/uk/gov/hmcts/reform/em/annotation/functional/AuthTokenProvider.java
+++ functionalTests/src/functionalTest/java/uk/gov/hmcts/reform/em/annotation/functional/AuthTokenProvider.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.hamcrest.CoreMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,17 +16,24 @@ import java.util.Base64;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.junit.Assert.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Service
 public class AuthTokenProvider {
 
     private final String idamS2SBaseUri;
     private final String idamUserBaseUrl;
+    private final String s2sSecret;
 
     @Autowired
     public AuthTokenProvider(@Value("${base-urls.idam-s2s}") String idamS2SBaseUri,
-                             @Value("${base-urls.idam-user}") String idamUserBaseUri) {
+                             @Value("${base-urls.idam-user}") String idamUserBaseUri,
+                             @Value("${base-urls.s2s-token}") String s2sSecret) {
         this.idamS2SBaseUri = idamS2SBaseUri;
         this.idamUserBaseUrl = idamUserBaseUri;
+        this.s2sSecret = s2sSecret;
         System.out.println("IDAM User URL - " + idamUserBaseUri);
         System.out.println("IDAM S2S URL - " + idamS2SBaseUri);
     }
@@ -36,34 +45,48 @@ public class AuthTokenProvider {
 
     public void createIdamUser(String email, String password, Optional<String> maybeRole) throws JsonProcessingException {
         ImmutableMap<String, Object> body = ImmutableMap.of("email", email,
-                "forename", "test",
-                "surname", "test",
-                "password", password,
-                "roles", ImmutableList.of(ImmutableMap.of("code", maybeRole.orElse("citizen"),
-                        "displayName", maybeRole.orElse("Citizen"))));
+            "forename", "test",
+            "surname", "test",
+            "password", password,
+            "roles", ImmutableList.of(ImmutableMap.of("code", maybeRole.orElse("citizen"),
+                "displayName", maybeRole.orElse("Citizen"))));
 
         RestAssured
-                .given().log().all().baseUri(idamUserBaseUrl)
-                .body(new ObjectMapper().writeValueAsBytes(body))
-                .contentType("application/json")
-                .post("testing-support/accounts")
-                .then()
-                .statusCode(204);
+            .given().log().all().baseUri(idamUserBaseUrl)
+            .body(new ObjectMapper().writeValueAsBytes(body))
+            .contentType("application/json")
+            .post("testing-support/accounts")
+            .then()
+            .statusCode(204);
     }
 
     public String findServiceToken() {
-        return RestAssured
-                .given().baseUri(idamS2SBaseUri)
-                .param("microservice", "em_gw")
-                .post("testing-support/lease")
-                .andReturn().asString();
+        Map<String, Object> params = ImmutableMap.of(
+            "microservice", "em_gw",
+            "oneTimePassword", new GoogleAuthenticator().getTotpPassword(this.s2sSecret)
+        );
+
+        Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(this.idamS2SBaseUri)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .body(params)
+            .post("/lease")
+            .andReturn();
+
+        assertThat(response.getStatusCode(), CoreMatchers.equalTo(200));
+
+        return "Bearer " + response
+            .getBody()
+            .print();
     }
 
     private String findUserToken(String email, String password) {
         final String encoded = Base64.getEncoder().encodeToString((email + ":" + password).getBytes());
         final Response authorization = RestAssured.given().baseUri(idamUserBaseUrl)
-                .header("Authorization", "Basic " + encoded)
-                .post("oauth2/authorize");
+            .header("Authorization", "Basic " + encoded)
+            .post("oauth2/authorize");
         authorization.then().statusCode(200);
         final Map<String, String> userResponse = authorization.andReturn().as(Map.class);
         return userResponse.get("access-token");
@@ -72,9 +95,9 @@ public class AuthTokenProvider {
     public int findUserId(String userToken) {
         final Response details =
             RestAssured.given()
-            .baseUri(idamUserBaseUrl)
-            .header("Authorization", "Bearer " + userToken)
-            .get("details");
+                .baseUri(idamUserBaseUrl)
+                .header("Authorization", "Bearer " + userToken)
+                .get("details");
 
         details.then().statusCode(200);
         return details.path("id");
@@ -82,9 +105,9 @@ public class AuthTokenProvider {
 
     public void deleteUser(String probateCaseworkerEmail) {
         RestAssured.given().log().all().baseUri(idamUserBaseUrl)
-                .delete("testing-support/accounts/" + probateCaseworkerEmail)
-                .andReturn()
-                .getStatusCode();
+            .delete("testing-support/accounts/" + probateCaseworkerEmail)
+            .andReturn()
+            .getStatusCode();
     }
 
     public class AuthTokens {
